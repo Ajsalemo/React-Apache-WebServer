fda8d8fceadc7f7d059f368cdac4e6f50979dc4d
Anthony Salemo <ajssalemo@gmail.com>
2021-11-19 18:01:33 -0500

    got a basic example of react being served by apache httpd in a container

---
+++ react-apache-webserver-containerized/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders React-Apache-Webserver-Containerized', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/React-Apache-Webserver-Containerized/i);
+  expect(linkElement).toBeInTheDocument();
+});
